d915f2d1ddda578c9bb2549ce7d6a0f49a048529
MarcosNovella <marcosnovella99@gmail.com>
2023-05-18 19:55:58 -0300

    changes for deploy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const loadDataFromApi = require("./src/dbload/loadDataBase");
+require('dotenv').config();
+const { PORT } = process.env
 
 conn.sync({ alter: true })
     .then(() => {
-        server.listen(3001, () => {
-            console.log('server listening at 3001');
+        server.listen(PORT, () => {
+            console.log(`server listening at ${PORT}`);
         });
     })
     .then(() => {
